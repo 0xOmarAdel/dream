22b227f49eebe30572adbec7732c801b3ac20a5f
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-10-28 17:05:27 +0200

    Navbar instead of Nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Fragment } from "react";
-import Nav from "./components/navbar";
+import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
@@ -10,16 +9,14 @@ import OrderOnline from "./pages/OrderOnline";
 const App = () => {
   return (
     <Fragment>
-      <Nav />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/reservations" element={<Reservations />} />
-          <Route path="/OrderOnline" element={<OrderOnline />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/OrderOnline" element={<OrderOnline />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Fragment>
   );
 };
