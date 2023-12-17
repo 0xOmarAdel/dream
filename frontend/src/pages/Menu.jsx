9647729c6cc28c79e563cd83efc0734c37902450
Heisenberg <omaradelawad20013@gmail.com>
2023-12-17 18:53:08 +0200

    Updated Menu page

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,7 +1,20 @@
+import Banner from "../layout/Banner";
+import Meals from "../components/Meals";
+import MenuFilters from "../components/MenuFilters";
+
 const Menu = () => {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="flex flex-col gap-10">
+      <Banner />
+      <div className="px-20 grid grid-cols-5 gap-10">
+        <MenuFilters />
+        <div className="col-span-4">
+          <div className="flex flex-row justify-between">
+            <h1 className="text-3xl text-gray-800 font-semibold">Menu</h1>
+          </div>
+          <Meals />
+        </div>
+      </div>
     </div>
   );
 };
