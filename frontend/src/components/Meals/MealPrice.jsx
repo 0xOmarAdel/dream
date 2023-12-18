ba8a1b2e4e61152477320b41510264687a5b1f10
Heisenberg <omaradelawad20013@gmail.com>
2023-12-18 11:41:13 +0200

    created a meal price component

---
+++ frontend/src/components/Meals/MealPrice.jsx
@@ -0,0 +1,11 @@
+const MealPrice = () => {
+  return (
+    <div className="mt-1 flex flex-row gap-2 text-lg font-semibold">
+      <span>$20</span>
+      <span>-</span>
+      <span>$70</span>
+    </div>
+  );
+};
+
+export default MealPrice;
