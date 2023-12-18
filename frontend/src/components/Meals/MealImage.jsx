0ce86cb92974e263f666d7f0369fd29c699b6a2c
Heisenberg <omaradelawad20013@gmail.com>
2023-12-18 11:44:31 +0200

    created a meal image component

---
+++ frontend/src/components/Meals/MealImage.jsx
@@ -0,0 +1,11 @@
+const MealImage = () => {
+  return (
+    <img
+      src="https://cafeu.vercel.app/img/product/12.png"
+      alt=""
+      className="w-full bg-gray-200 rounded-md"
+    />
+  );
+};
+
+export default MealImage;
