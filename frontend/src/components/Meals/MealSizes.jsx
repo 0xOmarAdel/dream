9b0133909f6c832f4d6a78c5c8c740ba1d3e374d
Heisenberg <omaradelawad20013@gmail.com>
2023-12-18 11:43:12 +0200

    created a meal sizes component

---
+++ frontend/src/components/Meals/MealSizes.jsx
@@ -0,0 +1,11 @@
+const MealSizes = () => {
+  return (
+    <div className="mt-2 flex flex-row gap-1.5 text-sm">
+      <span className="border px-1 py-0.5">S</span>
+      <span className="border px-1 py-0.5">M</span>
+      <span className="border px-1 py-0.5">L</span>
+    </div>
+  );
+};
+
+export default MealSizes;
