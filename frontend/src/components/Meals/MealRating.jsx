ae8f43d9015d2f158e9d4f246ce9656149742858
Heisenberg <omaradelawad20013@gmail.com>
2023-12-18 11:44:38 +0200

    created a meal rating component

---
+++ frontend/src/components/Meals/MealRating.jsx
@@ -0,0 +1,15 @@
+import { FaStar } from "react-icons/fa6";
+
+const MealRating = () => {
+  return (
+    <div className="mt-3 flex flex-row gap-1 text-lg text-primary">
+      <FaStar />
+      <FaStar />
+      <FaStar />
+      <FaStar />
+      <FaStar />
+    </div>
+  );
+};
+
+export default MealRating;
