75b19d022f42f2e57e8f97bfd669b0409bb806d5
Pegasus <nwillson2601@gmail.com>
2023-12-20 08:52:33 +0200

    edited Navbar // edited reservation system// edited Hero

--- frontend/src/components/ResrevationsSystem.jsx
+++ frontend/src/components/ResrevationsSystem.jsx
@@ -1,6 +1,7 @@
 // ReservationSystem.js
 
 import React, { useState } from "react";
+import tableicon from "../assets/table.png";
 
 const ReservationSystem = () => {
   const [selectedSeats, setSelectedSeats] = useState([]);
@@ -33,6 +34,7 @@ const ReservationSystem = () => {
           >
             <div className="table">
               <div className={`chair ${isSelected ? "selected-chair" : ""}`} />
+              <img src={tableicon} alt="table" />
             </div>
             <span className="seat-number">{seatNumber}</span>
           </div>
@@ -44,14 +46,25 @@ const ReservationSystem = () => {
   };
 
   return (
-    <div className="reservation-system ">
-      <h2>Restaurant Table Reservation</h2>
-      <div className="seat-container ">{renderSeats()}</div>
-      <div className="selected-seats">
-        <h3>Selected Seats</h3>
-        <p>{selectedSeats.join(", ")}</p>
+    <>
+      <div className="reservation-system">
+        <div className="hero min-h-screen flex items-center justify-center text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-medium text-sky-500 mb-4 pb-7">
+              Choose Your Table
+            </h1>
+            <div className="seat-container grid grid-cols-4 gap-4 mt-4">
+              {renderSeats()}
+            </div>
+            <div className="selected-seats mt-4">
+              <h3>Selected Seats</h3>
+              <p>{selectedSeats.join(", ")}</p>
+            </div>
+            <button className="btn btn-primary mt-4">Confirm</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -8,29 +8,156 @@ import ShowCartModalButton from "../components/Cart/ShowCartModalButton";
 
 const Navbar = () => {
   return (
-    <nav className="navbar px-20 items-center justify-between font-medium py-5">
-      <Link to="/" className="normal-case text-2xl text-sky-500">
-        Dream
-      </Link>
-      <ul className="text-xl hidden lg:flex lg:space-x-4">
-        {navLinks.map((navLink) => (
-          <NavItem key={navLink.id} to={navLink.to} text={navLink.text} />
-        ))}
-      </ul>
-      <div className="flex flex-row gap-4">
-        <div className="flex flex-row gap-1">
-          <AiOutlineUser className="text-2xl" />
-          <span>Login</span>
-        </div>
-        <ShowCartModalButton>
-          <div className="flex flex-row items-center gap-1">
-            <IoCartOutline className="text-2xl" />
-            <span>Cart</span>
-          </div>
-        </ShowCartModalButton>
-        <CartModal />
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link
+          to="/"
+          className="btn btn-ghost text-xl text-sky-500 font-semibold"
+        >
+          Dream
+        </Link>
       </div>
-    </nav>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <a>Order Online</a>
+                <ul className="p-2">
+                  <li>
+                    <Link to="/menu">Menu</Link>
+                  </li>
+                  <li>
+                    <Link to="/reservations">Reservations</Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Link>Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/" className="normal-case text-xl ">
+                Home
+              </Link>
+            </li>
+            <li>
+              <details>
+                <summary className="normal-case text-xl ">Order Online</summary>
+                <ul className="p-2">
+                  <li>
+                    <Link to="/menu" className="normal-case text-xl ">
+                      Menu
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/reservations" className="normal-case text-xl ">
+                      Reservations
+                    </Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <Link className="normal-case text-xl ">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end"></div>
+      </div>
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          <div
+            tabIndex={0}
+            className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+          >
+            <div className="card-body">
+              <span className="font-bold text-lg">8 Items</span>
+              <span className="text-info">Subtotal: $999</span>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
