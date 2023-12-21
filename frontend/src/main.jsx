5625ad61a03a3a04f34d3204ade94c88054c8636
Heisenberg <omaradelawad20013@gmail.com>
2023-12-21 10:43:13 +0200

    added redux store to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import "./index.css";
+
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
