1ab81a13f8d0d2552add182ed4da7eba2596ad26
Pegasus <nwillson2601@gmail.com>
2023-12-21 05:46:30 +0200

    converted mealSizes to button

--- frontend/src/components/Meals/MealSizes.jsx
+++ frontend/src/components/Meals/MealSizes.jsx
@@ -1,9 +1,38 @@
+import React, { useState } from "react";
+
 const MealSizes = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setActiveButton(index);
+  };
+
   return (
     <div className="mt-2 flex flex-row gap-1.5 text-sm">
-      <span className="border px-1 py-0.5">S</span>
-      <span className="border px-1 py-0.5">M</span>
-      <span className="border px-1 py-0.5">L</span>
+      <button
+        className={`${
+          activeButton === 0 ? "text-sky-500" : "text-gray-600"
+        } hover:bg-sky-500`}
+        onClick={() => handleButtonClick(0)}
+      >
+        <span className="border px-1 py-0.5">S</span>
+      </button>
+      <button
+        className={`${
+          activeButton === 1 ? "text-sky-500" : "text-gray-600"
+        } hover:bg-sky-500`}
+        onClick={() => handleButtonClick(1)}
+      >
+        <span className="border px-1 py-0.5">M</span>
+      </button>
+      <button
+        className={`${
+          activeButton === 2 ? "text-sky-500" : "text-gray-600"
+        } hover:bg-sky-500`}
+        onClick={() => handleButtonClick(2)}
+      >
+        <span className="border px-1 py-0.5">L</span>
+      </button>
     </div>
   );
 };
