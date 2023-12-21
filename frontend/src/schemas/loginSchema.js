803b7597102e0199c2deb00f5c04dd8c148438c6
Heisenberg <omaradelawad20013@gmail.com>
2023-12-21 14:04:19 +0200

    updated yup log in schema

---
+++ frontend/src/schemas/loginSchema.js
@@ -0,0 +1,9 @@
+import * as Yup from "yup";
+
+export const logInSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
