e412fe29947a41dff9fe14197c1525b784e24057
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-23 01:23:58 +0200

    make it fully dynamic - change price after clicking on size

--- frontend/src/components/Meals/Meal.jsx
+++ frontend/src/components/Meals/Meal.jsx
@@ -4,17 +4,20 @@ import MealSizes from "./MealSizes";
 import MealImage from "./MealImage";
 import MealRating from "./MealRating";
 import MealAddToCartButton from "./MealAddToCartButton";
+import { useState } from "react";
 
 const Meal = ({ title, description, options, image, _id, categoryId }) => {
-  const handleSizeClick = (selectedSize) => {
-    console.log("Selected Size:", selectedSize);
+  const [selectedSize, setSelectedSize] = useState(options[0].size);
+
+  const handleSizeClick = (size) => {
+    setSelectedSize(size);
   };
   return (
-    <div className="col-span-1 flex flex-col items-center p-5 shadow-md rounded-md text-center">
+    <div className="col-span-1 flex flex-col items-center p-3 shadow-md rounded-md text-center">
       <MealImage image={image} />
       <MealTitle title={title} />
       <MealRating />
-      <MealPrice options={options} />
+      <MealPrice options={options} selectedSize={selectedSize} />
       <MealSizes options={options} onSizeClick={handleSizeClick} />
       <MealAddToCartButton />
     </div>
