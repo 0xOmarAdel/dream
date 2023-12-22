9a4d3582c1706785683040aa4498174910903c12
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-23 01:03:33 +0200

    first time using axiosAPI with its base url

--- frontend/src/components/Meals/Meals.jsx
+++ frontend/src/components/Meals/Meals.jsx
@@ -1,11 +1,23 @@
 import Meal from "./Meal";
-import { meals } from "../../data/fakeProducts";
+import { useEffect, useState } from "react";
+import axiosApi from "../../utils/axiosConfig";
 
 const Meals = () => {
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    axiosApi
+      .get("/meals")
+      .then((response) => {
+        setMeals(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching meals:", error);
+      });
+  }, []);
   return (
     <div className="grid grid-cols-5 gap-8">
       {meals.map((meal) => (
-        <Meal key={meal.id} {...meal} />
+        <Meal key={meal._id} {...meal} />
       ))}
     </div>
   );
