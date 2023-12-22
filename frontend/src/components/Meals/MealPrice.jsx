91c33fa8ce206b3c86c68ddb562da8fce4317755
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-23 01:24:08 +0200

    make it fully dynamic - change price after clicking on size

--- frontend/src/components/Meals/MealPrice.jsx
+++ frontend/src/components/Meals/MealPrice.jsx
@@ -1,11 +1,9 @@
-const MealPrice = ({ options }) => {
-  const maxPrice = Math.max(...options.map((option) => option.price));
-  const minPrice = Math.min(...options.map((option) => option.price));
+const MealPrice = ({ options, selectedSize }) => {
+  const selectedOption = options.find((option) => option.size === selectedSize);
+
   return (
     <div className="mt-1 flex flex-row gap-2 text-lg font-semibold">
-      <span>${minPrice}</span>
-      <span>-</span>
-      <span>${maxPrice}</span>
+      <span>${selectedOption ? selectedOption.price : 0}</span>
     </div>
   );
 };
