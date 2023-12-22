f0432de71a90fb3c5778c3cb86cc4a786030caf4
Heisenberg <omaradelawad20013@gmail.com>
2023-12-22 13:36:19 +0200

    removed the "new" badge in the profile link in the navbar

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -148,10 +148,7 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <Link to="/profile" className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </Link>
+              <Link to="/profile">Profile</Link>
             </li>
             <li>
               <Link to="/orders">Orders</Link>
