a8813cbd369b83b5a8de5b642ac273d1fef1c424
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-24 09:04:10 +0200

    Orders Page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,5 +1,19 @@
+import Order from "../components/Order/Order";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <>
+      <div className="flex justify-start item-start space-y-2 flex-col">
+        <h1 className="ml-6 mt-3 text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-primary">
+          My Orders
+        </h1>
+        <div className="border-gray-200 border-b-2 w-24 ml-6"></div>
+      </div>
+
+      <Order OrderStat="Pending" />
+      <Order OrderStat="Delivered" />
+    </>
+  );
 };
 
 export default Orders;
