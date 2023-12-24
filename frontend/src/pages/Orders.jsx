2fa0a1d4772774034f6cd54bccc6b4a2bc706142
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-24 09:08:24 +0200

    ui change

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -3,11 +3,11 @@ import Order from "../components/Order/Order";
 const Orders = () => {
   return (
     <>
-      <div className="flex justify-start item-start space-y-2 flex-col">
+      <div className="flex justify-start item-start space-y-4 flex-col">
         <h1 className="ml-6 mt-3 text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-primary">
           My Orders
         </h1>
-        <div className="border-gray-200 border-b-2 w-24 ml-6"></div>
+        <div className="border-gray-200 border-b-4 w-24 ml-6"></div>
       </div>
 
       <Order OrderStat="Pending" />
