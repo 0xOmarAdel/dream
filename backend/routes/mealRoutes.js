d6b6163996a3aaae4bd213f5de2f8119fb5e29e2
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-25 10:56:49 +0200

    added rateMeal and Get Meal Rating Info

--- backend/routes/mealRoutes.js
+++ backend/routes/mealRoutes.js
@@ -5,12 +5,17 @@ const {
   getSingleMeal,
   editMeal,
   deleteMeal,
+  rateMeal,
+  getMealRatingInfo,
 } = require("../controllers/mealController");
+const authenticateUser = require("../middleware/authenticateUser");
 
 router.post("/", createMeal);
 router.get("/", getAllMeals);
 router.get("/:id", getSingleMeal);
 router.put("/:id", editMeal);
 router.delete("/:id", deleteMeal);
+router.post("/:id/rate", authenticateUser, rateMeal);
+router.get("/:id/rating-info", getMealRatingInfo);
 
 module.exports = router;
