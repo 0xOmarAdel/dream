65661dc65daa60b836f80e337cecd9b1ef36aeb9
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-25 10:56:35 +0200

    added rateMeal and Get Meal Rating Info

--- backend/controllers/mealController.js
+++ backend/controllers/mealController.js
@@ -1,4 +1,5 @@
 const Meal = require("../models/Meal");
+const Review = require("../models/Review");
 
 const createMeal = async (req, res) => {
   try {
@@ -70,10 +71,77 @@ const deleteMeal = async (req, res) => {
   }
 };
 
+const rateMeal = async (req, res) => {
+  const { id } = req.params;
+  const { rating } = req.body;
+  const userId = req.user._id;
+  try {
+    const meal = await Meal.findById(id);
+
+    if (!meal) {
+      return res.status(404).json({ error: "Meal not found" });
+    }
+
+    const existingReview = await Review.findOne({ user: userId, meal: id });
+
+    if (existingReview) {
+      return res.status(400).json({ error: "User already rated this meal" });
+    }
+
+    const newReview = new Review({
+      user: userId,
+      meal: id,
+      rating: rating,
+    });
+
+    await newReview.save();
+
+    meal.reviews.push(newReview);
+    await meal.save();
+
+    res.status(200).json({ message: "Rating added successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getMealRatingInfo = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const meal = await Meal.findById(id).populate("reviews");
+
+    if (!meal) {
+      return res.status(404).json({ error: "Meal not found" });
+    }
+
+    const totalRatings = meal.reviews.length;
+    const averageRating = totalRatings
+      ? meal.reviews.reduce((sum, review) => sum + review.rating, 0) /
+        totalRatings
+      : 0;
+
+    const stars = [0, 0, 0, 0, 0];
+    meal.reviews.forEach((review) => {
+      stars[review.rating - 1]++;
+    });
+
+    res.status(200).json({
+      averageRating,
+      totalRatings,
+      stars,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createMeal,
   getAllMeals,
   getSingleMeal,
   editMeal,
   deleteMeal,
+  rateMeal,
+  getMealRatingInfo,
 };
