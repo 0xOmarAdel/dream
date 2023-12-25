e4c14af510cb204ff4a6beee7795a73a53d2db26
Heisenberg <omaradelawad20013@gmail.com>
2023-12-25 18:13:19 +0200

    created a cart slice and added it to rtk store

---
+++ frontend/src/store/slices/cartSlice.jsx
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialCartState = {
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+};
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
+  try {
+    const cartItems = await axios.get("http://localhost:5000/api/v1/cart");
+
+    console.log(cartItems);
+
+    let totalQuantity = 0;
+    let totalPrice = 0;
+
+    for (const cartItem of cartItems) {
+      totalPrice += cartItem.price * cartItem.quantity;
+      totalQuantity += cartItem.quantity;
+    }
+
+    return {
+      cartItems,
+      totalQuantity,
+      totalPrice,
+    };
+  } catch (error) {
+    console.log("An error occurred!");
+  }
+});
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialCartState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCartItems.fulfilled, (state, action) => {
+      state.items = action.payload.cartItems;
+      state.totalQuantity = action.payload.totalQuantity;
+      state.totalPrice = action.payload.totalPrice;
+    });
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/userAuthSlice";
+import cartSlice from "./slices/cartSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    cart: cartSlice,
   },
 });
