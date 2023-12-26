8fc27a346af0ec17ca6c5168420d08127cd6a6a3
Heisenberg <omaradelawad20013@gmail.com>
2023-12-26 12:10:56 +0200

    fixed the navbar menu profile route

--- frontend/src/data/userMenu.js
+++ frontend/src/data/userMenu.js
@@ -1,5 +1,5 @@
 export const userMenu = [
-  { id: 1, text: "profile", to: "/account" },
+  { id: 1, text: "profile", to: "/profile" },
   { id: 2, text: "orders", to: "/orders" },
   { id: 3, text: "reservations", to: "/reservations" },
 ];
