ec2c6e18e1b93a2bb3fe176bdd47a94b65e2ce7e
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-27 10:53:47 +0200

    added weeklyspecials as i re-designed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import Hero from "../components/Hero";
 import Specials from "../components/WeeklySpecials";
 import Testimonials from "../components/Testimonials";
 import About from "../components/AboutRestaurant";
+import WeeklySpecials from "../components/WeeklySpecials";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <WeeklySpecials />
     </>
   );
 };
