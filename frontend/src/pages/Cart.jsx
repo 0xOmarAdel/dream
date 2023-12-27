d2921226068fc781805c88a2246cffb60797e773
Heisenberg <omaradelawad20013@gmail.com>
2023-12-27 15:32:28 +0200

    updated cart page static text

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import CartItems from "../components/CartItems/CartItems";
 import { shippingFees } from "../data/shippingFees";
 import { useSelector } from "react-redux";
+import { IoCartOutline } from "react-icons/io5";
 
 const Cart = () => {
   const totalPrice = useSelector((state) => state.cart.totalPrice);
@@ -9,12 +10,15 @@ const Cart = () => {
     <section className="py-24 bg-gray-100 font-poppins">
       <div className="px-24 py-6 lg:py-4 md:px-24">
         <div className="flex flex-col xl:flex-row  gap-8">
-          <div className=" grow p-6 mb-8 border bg-gray-50 dark:border-sky-100">
-            <h2 className="mb-8 text-4xl font-bold text-sky-500">Your Cart</h2>
+          <div className="grow p-6 border bg-gray-50">
+            <h2 className="flex flex-row items-center gap-3 mb-8 text-3xl text-primary font-medium">
+              <IoCartOutline className="text-4xl" />
+              Shopping Cart
+            </h2>
             <CartItems />
           </div>
           <div className="min-w-[20%] p-6 border border-blue-100 bg-gray-50 md:p-8">
-            <h2 className="mb-8 text-3xl font-bold text-sky-500">
+            <h2 className="mb-8 text-3xl text-primary font-medium">
               Order Summary
             </h2>
             <div className="flex items-center justify-between pb-4 mb-4 border-b border-gray-300  ">
