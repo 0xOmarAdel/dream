bbb5398355ad5b13235b10ce4ea20ed8d384d88c
Heisenberg <omaradelawad20013@gmail.com>
2023-12-27 17:22:56 +0200

    added the card components to the cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,23 +2,24 @@ import { IoCartOutline } from "react-icons/io5";
 import CartItems from "../components/CartItems/CartItems";
 import CartSummary from "../components/CartSummary/CartSummary";
 import PaymentMethods from "../components/PaymentMethods";
+import Card from "../ui/Card";
+import CardTitle from "../ui/CardTitle";
 
 const Cart = () => {
   return (
     <section className="py-24 bg-gray-100 font-poppins">
       <div className="px-24 py-6 lg:py-4 md:px-24">
         <div className="flex flex-col xl:flex-row gap-12">
-          <div className="grow h-fit p-8 pb-10 border bg-gray-50 rounded-md shadow-md">
-            <h2 className="flex flex-row items-center gap-3 mb-8 text-3xl text-primary font-medium">
-              <IoCartOutline className="text-4xl" />
-              Shopping Cart
-            </h2>
+          <Card classes="grow">
+            <CardTitle
+              title="Shopping Cart"
+              icon={IoCartOutline}
+              iconClasses="text-4xl"
+            />
             <CartItems />
-          </div>
-          <div className="min-w-[20%] h-fit p-8 pb-10 border bg-gray-50 rounded-md shadow-md">
-            <h2 className="mb-8 text-3xl text-primary font-medium">
-              Order Summary
-            </h2>
+          </Card>
+          <Card classes="min-w-[20%]">
+            <CardTitle title="Order Summary" />
             <div className="flex flex-col gap-4">
               <CartSummary />
               <PaymentMethods />
@@ -26,7 +27,7 @@ const Cart = () => {
                 Checkout
               </button>
             </div>
-          </div>
+          </Card>
         </div>
       </div>
     </section>
