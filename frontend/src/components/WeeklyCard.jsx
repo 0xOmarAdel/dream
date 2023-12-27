111f7261cd7021a43a2668401d92b621fdb32133
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-27 10:53:02 +0200

    re-design weekly card but still static

--- frontend/src/components/WeeklyCard.jsx
+++ frontend/src/components/WeeklyCard.jsx
@@ -1,21 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
 const WeeklyCard = ({ image, title, description }) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img className="w-full" src={image} alt="Steak plate" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>{description}</p>
-        <div className="card-actions justify-end">
-          <Link
-            to="/shop"
-            className="btn btn-primary bg-sky-500 hover:bg-sky-500 text-white border-none"
-          >
-            Order Now
-          </Link>
+    <div className="w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden">
+      <div className="max-w-md mx-auto">
+        <div className="p-4 sm:p-6">
+          <div className="h-[236px] bg-cover bg-center bg-[url('https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/11/19/1/WU0311H_rib-eye-steak-with-onion-blue-cheese-sauce-recipe_s4x3.jpg.rend.hgtvcom.1280.1280.suffix/1382542189320.jpeg')]"></div>
+          <p className="font-bold text-gray-700 text-[22px] leading-7 mb-1">
+            Steak with Cheese
+          </p>
+          <div className="flex flex-row">
+            <p className="text-[17px] font-bold text-sky-500">Meat idk</p>
+          </div>
+          <p className="text-[#7C7C80] font-[15px] mt-2">
+            Our shrimp sauce is made with mozarella, a creamy taste of shrimp
+            with extra kick of spices.
+          </p>
+          <button className="block mt-6 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform bg-sky-500 rounded-[14px] hover:bg-sky-600 focus:outline-none text-white">
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
