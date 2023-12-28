0ff47ff72b66dfb02bb0a440aad0da80fc13ca12
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-28 10:47:23 +0200

    Added About Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../layout/Hero";
 import WeeklySpecials from "../components/WeeklySpecials";
+import About from "../components/AboutRestaurant";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <WeeklySpecials />
+      <About />
     </>
   );
 };
