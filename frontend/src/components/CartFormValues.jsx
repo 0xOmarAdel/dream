164f92f0417fb622605b09abcc40cb34dd64ca2e
Heisenberg <omaradelawad20013@gmail.com>
2023-12-28 11:16:11 +0200

    created CartFormValues component

---
+++ frontend/src/components/CartFormValues.jsx
@@ -0,0 +1,12 @@
+import { addressFormatter } from "../utils/addressFormatter";
+
+const CartFormValues = ({ formikValues }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <p>{addressFormatter(formikValues)}</p>
+      <p>{formikValues.phone}</p>
+    </div>
+  );
+};
+
+export default CartFormValues;
