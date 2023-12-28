05a07b6897fff0f03679ebcc31e6b985ddb988ff
Pegasus <nwillson2601@gmail.com>
2023-12-28 13:33:22 +0200

    editing Reservation page with Formik

--- frontend/src/pages/Reservations.jsx
+++ frontend/src/pages/Reservations.jsx
@@ -1,6 +1,36 @@
+import { useFormik } from "formik";
 import ReservationSystem from "../components/ResrevationsSystem";
 
 const Reservations = () => {
+  const formik = useFormik({
+    initialValues: {
+      resDate: "",
+      resTime: "",
+      guests: "",
+      occasion: "",
+      name: "",
+      email: "",
+      phone: "",
+      agree: false,
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.resDate) {
+        errors.resDate = "Required";
+      }
+      if (!values.resTime) {
+        errors.resTime = "Required";
+      }
+      if (!values.guests) {
+        errors.guests = "Required";
+      }
+      return errors;
+    },
+  });
+
   return (
     <div className="flex justify-center items-center w-full  min-h-screen bg-white px-5 py-5">
       <div className="xl:max-w-7xl bg-white w-full rounded-md flex justify-between items-stretch px-5 xl:px-5 py-5">
@@ -8,18 +38,26 @@ const Reservations = () => {
           <h1 className="text-start text-4xl sm:text-3xl font-semibold text-sky-500">
             Booking
           </h1>
-          <div className="w-full mt-5 sm:mt-8">
+          <form className="w-full mt-5 sm:mt-8">
             <div className="mx-auto w-full sm:max-w-md md:max-w-lg flex flex-col gap-5">
               <div className="flex flex-col sm:flex-row gap-3">
                 <label className="">Choose Date</label>
                 <input
+                  id="resDate"
+                  name="resDate"
+                  value={formik.values.resDate}
+                  onChange={formik.handleChange}
                   type="date"
-                  id="res-date"
                   className="input input-bordered input-primary w-full max-w-xs bg-white text-black placeholder:text-black/70"
                 />
                 <label>Choose Time</label>
                 <select
-                  id="res-time"
+                  id="resTime"
+                  name="resTime"
+                  value={formik.values.resTime}
+                  onChange={formik.handleChange}
+                  type="time"
+                  placeholder="Choose Time"
                   className="input input-bordered input-primary w-full max-w-xs bg-white text-black placeholder:text-black/70"
                 >
                   <option>17:00</option>
@@ -32,41 +70,57 @@ const Reservations = () => {
               </div>
               <label>Number of Guests</label>
               <input
+                id="guests"
+                name="guests"
+                value={formik.values.guests}
+                onChange={formik.handleChange}
                 type="number"
                 placeholder="1"
-                min="1"
-                max="10"
-                id="guests"
                 className="input input-bordered input-primary w-full bg-white text-black placeholder:text-black/70"
               />
               <div className="flex flex-col sm:flex-row gap-3">
                 <input
+                  id="name"
+                  name="name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
                   type="text"
-                  placeholder="Enter Your First Name"
+                  placeholder="Enter Your Name"
                   className="input input-bordered input-primary w-full max-w-xs bg-white text-black placeholder:text-black/70"
                 />
                 <input
+                  id="occasion"
+                  name="occasion"
+                  value={formik.values.occasion}
+                  onChange={formik.handleChange}
                   type="text"
-                  placeholder="Enter Your Last Name"
+                  placeholder="Occasion (optional)"
                   className="input input-bordered input-primary w-full max-w-xs bg-white text-black placeholder:text-black/70"
                 />
               </div>
               <input
+                id="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
                 type="text"
                 placeholder="Enter Your Email"
                 className="input input-bordered input-primary w-full bg-white text-black placeholder:text-black/70"
               />
               <input
-                type="text"
+                id="phone"
+                name="phone"
+                value={formik.values.phone}
+                onChange={formik.handleChange}
+                type="number"
                 placeholder="Enter Your Phone No"
                 className="input input-bordered input-primary w-full bg-white text-black placeholder:text-black/70"
               />
               <input
-                type="text"
-                placeholder="Enter Your Address"
-                className="input input-bordered input-primary w-full bg-white text-black placeholder:text-black/70"
-              />
-              <input
+                id="comment"
+                name="comment"
+                value={formik.values.comment}
+                onChange={formik.handleChange}
                 type="text"
                 placeholder="Comment (optional)"
                 className="input input-bordered input-primary w-full bg-white text-black placeholder:text-black/70"
@@ -75,6 +129,11 @@ const Reservations = () => {
                 <div className="form-control">
                   <label className="label cursor-pointer">
                     <input
+                      name="agree"
+                      onChange={formik.handleChange}
+                      value={formik.values.agree}
+                      checked={formik.values.agree}
+                      id="agree"
                       type="checkbox"
                       className="checkbox-xs checkbox-primary bg-white"
                     />
@@ -93,7 +152,7 @@ const Reservations = () => {
                 </button>
               </div>
             </div>
-          </div>
+          </form>
         </div>
         <div className="sm:w-[60%] lg:w-[50%] bg-cover bg-center items-center justify-center hidden md:flex ">
           <ReservationSystem />
