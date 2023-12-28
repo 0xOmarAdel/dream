a0c9aa4fae694934274378cf33c307a1b3526f37
Heisenberg <omaradelawad20013@gmail.com>
2023-12-28 12:07:21 +0200

    added them in the cart summary component

--- frontend/src/components/CartSummary/CartSummary.jsx
+++ frontend/src/components/CartSummary/CartSummary.jsx
@@ -3,12 +3,17 @@ import CartSummaryCartTotal from "./CartSummaryCartTotal";
 import CartSummaryShippingFees from "./CartSummaryShippingFees";
 import CartSummaryTotal from "./CartSummaryTotal";
 import Divider from "../../ui/Divider";
+import OrderSummaryAddress from "./OrderSummaryAddress";
+import OrderSummaryPhone from "./OrderSummaryPhone";
 
-const CartSummary = () => {
+const CartSummary = ({ formikValues }) => {
   const totalPrice = useSelector((state) => state.cart.totalPrice);
 
   return (
     <div className="flex flex-col gap-3 text-gray-700 font-medium">
+      <OrderSummaryAddress formikValues={formikValues} />
+      <OrderSummaryPhone phone={formikValues.phone} />
+      <Divider />
       <CartSummaryCartTotal totalPrice={totalPrice} />
       <CartSummaryShippingFees />
       <Divider />
