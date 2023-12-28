53a3b175e22e22c96842c478ff3d27f28a401269
Pegasus <nwillson2601@gmail.com>
2023-12-28 14:00:54 +0200

    edited reservationSchema

--- frontend/src/schemas/reservationsSchema.js
+++ frontend/src/schemas/reservationsSchema.js
@@ -1,5 +1,19 @@
 import * as Yup from "yup";
 
 export const reservationsSchema = Yup.object().shape({
-  name: Yup.string().required("Required"),
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  phone: Yup.string()
+    .min(10, "Invalid phone number")
+    .max(14, "Invalid phone number")
+    .required("Required"),
+  resDate: Yup.date().required("Required"),
+  resTime: Yup.string().required("Required"),
+  guests: Yup.string().required("Required"),
+  occasion: Yup.string(),
+  comments: Yup.string().min(2, "Too Short!").max(50, "Too Long!"),
+  table: Yup.string().required("Required"),
 });
