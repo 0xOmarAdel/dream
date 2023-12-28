ec6937e03e649d4c561004b64edc9ace4c50feb5
Pegasus <nwillson2601@gmail.com>
2023-12-29 00:16:58 +0200

    Edited reservation history // edited OrderHistory// installed Emailjs

--- frontend/src/components/Profile/OrderHistory.jsx
+++ frontend/src/components/Profile/OrderHistory.jsx
@@ -1,62 +1,34 @@
-import React from "react";
-import OrderStatus from "../Order/OrderStatus";
+// src/components/OrderHistory.js
+import React, { useState } from "react";
 
-const OrderHistory = ({ OrderStat }) => {
+const OrderHistory = () => {
+  const [orders, setOrders] = useState([]);
+
+  // Assume you have a way to fetch order history and update orders state accordingly
+
   return (
-    <>
-      <details className="collapse bg-base-200 ">
-        <summary className="collapse-title text-xl font-medium">
-          <h1 className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9  text-gray-800">
-            Order #53453 <OrderStatus status={OrderStat} />
-          </h1>
-          <p className="text-base font-medium leading-6 text-gray-600">
-            24 December 2023 at 01:23 PM
-          </p>
-        </summary>
-        <div className="collapse-content">
-          <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
-            <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6   ">
-              <h3 className="text-xl font-semibold leading-5 text-gray-800">
-                Summary
-              </h3>
-              <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
-                <div className="flex justify-between  w-full">
-                  <p className="text-base leading-4 text-gray-800">Subtotal</p>
-                  <p className="text-base leading-4 text-gray-600">$23.00</p>
+    <div className="max-w-2xl mx-auto mt-8 p-8 bg-white shadow-lg rounded-md">
+      <h2 className="text-3xl font-semibold mb-6">Order History</h2>
+      {orders.length === 0 ? (
+        <p className="text-gray-500">No orders yet.</p>
+      ) : (
+        <ul className="divide-y divide-gray-200">
+          {orders.map((order, index) => (
+            <li key={index} className="py-4">
+              <div className="flex items-center justify-between">
+                <div>
+                  <h3 className="text-lg font-semibold">{order.item}</h3>
+                  <p className="text-gray-500">
+                    {order.date.toDateString()} at {order.time}
+                  </p>
                 </div>
-
-                <div className="flex justify-between items-center w-full">
-                  <p className="text-base leading-4 text-gray-800">Shipping</p>
-                  <p className="text-base leading-4 text-gray-600">$5.00</p>
-                </div>
+                {/* Add additional details or actions if needed */}
               </div>
-              <div className="flex justify-between items-center w-full">
-                <p className="text-base font-semibold leading-4 text-gray-800">
-                  Total
-                </p>
-                <p className="text-base font-semibold leading-4 text-gray-600">
-                  $35.00
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </details>
-      <div className="divider"></div>
-      <details className="collapse bg-base-200">
-        <summary className="collapse-title text-xl font-medium">
-          <h1 className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9  text-gray-800">
-            Order #53453 <OrderStatus status={OrderStat} />
-          </h1>
-          <p className="text-base font-medium leading-6 text-gray-600">
-            24 December 2023 at 01:23 PM
-          </p>
-        </summary>
-        <div className="collapse-content">
-          <p>content</p>
-        </div>
-      </details>
-    </>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import UserInfo from "../components/Profile/UserInfo";
 import OrderHistory from "../components/Profile/OrderHistory";
+import ReservationHistory from "../components/Profile/ReservationHistory";
+import { MdOutlineHistory } from "react-icons/md";
+import { FaUserEdit } from "react-icons/fa";
+
 import { Link } from "react-router-dom";
 
 const user = {
@@ -13,6 +17,8 @@ const user = {
 const Profile = () => {
   const [isUserInfoVisible, setIsUserInfoVisible] = useState(false);
   const [isOrderHistoryVisible, setIsOrderHistoryVisible] = useState(false);
+  const [isReservationHistoryVisible, setIsReservationHistoryVisible] =
+    useState(false);
 
   const handleOrderHistoryClick = () => {
     setIsOrderHistoryVisible(!isOrderHistoryVisible);
@@ -24,15 +30,21 @@ const Profile = () => {
     setIsOrderHistoryVisible(false);
   };
 
+  const handleReservationHistoryClick = () => {
+    setIsReservationHistoryVisible(!isReservationHistoryVisible);
+    setIsUserInfoVisible(false);
+  };
+
   return (
     <>
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
+        <div className="drawer-content flex flex-col items-center justify-center bg-white">
           {/* Page content here */}
 
           {isUserInfoVisible && <UserInfo user={user} />}
           {isOrderHistoryVisible && <OrderHistory />}
+          {isReservationHistoryVisible && <ReservationHistory />}
           <label
             htmlFor="my-drawer-2"
             className="btn btn-primary drawer-button lg:hidden"
@@ -40,15 +52,15 @@ const Profile = () => {
             Open drawer
           </label>
         </div>
-        <div className="drawer-side">
+        <div className="drawer-side bg-white">
           <label
             htmlFor="my-drawer-2"
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
           {/* Sidebar content here */}
-          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-            <div className="menu bg-base-200 text-base-content">
+          <ul className="menu p-4 w-80 min-h-full bg-neutral-100 text-base-content">
+            <div className="menu bg-slate-50 text-base-content">
               <img
                 className="mask mask-circle"
                 src={user.profilePicture}
@@ -60,70 +72,28 @@ const Profile = () => {
             </div>
             <li>
               <button className="text-xl" onClick={handleUserInfoClick}>
+                <FaUserEdit />
                 Personal Information
               </button>
             </li>
             <li>
               <button className="text-xl" onClick={handleOrderHistoryClick}>
+                <MdOutlineHistory />
                 Order History
               </button>
             </li>
             <li>
-              <button className="text-xl">Reservations History</button>
+              <button
+                className="text-xl"
+                onClick={handleReservationHistoryClick}
+              >
+                <MdOutlineHistory />
+                Reservations History
+              </button>
             </li>
           </ul>
         </div>
       </div>
-
-      {/* <div className="hero-overlay bg-opacity-60">
-        <div className="drawer lg:drawer-open">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center justify-center"> */}
-      {/* Page content here */}
-      {/* {isUserInfoVisible && <UserInfo user={user} />}
-            {isOrderHistoryVisible && <OrderHistory />}
-            <label
-              htmlFor="my-drawer-2"
-              className="btn btn-primary drawer-button lg:hidden"
-            >
-              Open drawer
-            </label> */}
-      {/* </div>
-          <div className="drawer-side">
-            <label
-              htmlFor="my-drawer-2"
-              aria-label="close sidebar"
-              className="drawer-overlay"
-            ></label>
-            {/* Sidebar content here */}
-      {/* <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content"> */}
-      {/* <div className="menu bg-base-200 text-base-content">
-                <img
-                  className="mask mask-circle"
-                  src={user.profilePicture}
-                  alt={user.name}
-                />
-                <h1 className="text-3xl">{user.name}</h1>
-                <h2 className="text-xl">{user.email}</h2>
-                <div className="divider"></div>
-              </div>
-              <li>
-                <button className="text-xl" onClick={handleUserInfoClick}>
-                  Personal Information
-                </button>
-              </li>
-              <li>
-                <button className="text-xl" onClick={handleOrderHistoryClick}>
-                  Order History
-                </button>
-              </li>
-              <li>
-                <button className="text-xl">Reservations History</button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
