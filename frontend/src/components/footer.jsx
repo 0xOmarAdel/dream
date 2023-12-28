97d0f086782b5e6f4a9cf95dc9ce33124c6a8f9e
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-28 11:37:25 +0200

    ui and text editied

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -2,36 +2,38 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="footer border-t p-10 bg-gray-50 text-base-content">
+    <footer className="footer font-medium border-t p-10 bg-gray-50 text-base-content">
       <aside className="lg:px-4">
         <Link to="/" className="text-2xl text-sky-500 font-semibold">
           Dream
         </Link>
         <p>
-          ACME Industries Ltd.
+          A journey began with a passion for cooking.
           <br />
-          Providing reliable tech since 1992
+          Established in 2023
         </p>
       </aside>
       <nav>
-        <header className="footer-title">Services</header>
+        <header className="footer-title opacity-100">Services</header>
         <Link to="/menu" className="link link-hover">
           Menu
         </Link>
-        <a className="link link-hover">Reserve a Table</a>
+        <Link to="/reservations" className="link link-hover">
+          Reserve a Table
+        </Link>
         <a href="#weekly" className="link link-hover">
           Weekly specials
         </a>
       </nav>
       <nav>
-        <header className="footer-title">Company</header>
-        <a className="link link-hover">About us</a>
+        <header className="footer-title opacity-100">Company</header>
+        <a id="#about-us" className="link link-hover">
+          About us
+        </a>
         <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
       </nav>
       <nav>
-        <header className="footer-title">Legal</header>
+        <header className="footer-title opacity-100">Legal</header>
         <a className="link link-hover">Terms of use</a>
         <a className="link link-hover">Privacy policy</a>
         <a className="link link-hover">Cookie policy</a>
