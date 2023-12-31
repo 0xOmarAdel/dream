b449c4faee05b8aac851ee5464c53739f2dd6388
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-31 08:57:19 +0200

    getReservations added

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -29,6 +29,51 @@ const createReservation = async (req, res) => {
   }
 };
 
+const getReservations = async (req, res) => {
+  try {
+    let reservations;
+
+    if (req.user.role === "admin") {
+      reservations = await Reservation.find().populate("user");
+    } else {
+      reservations = await Reservation.find({ user: req.user._id }).populate(
+        "user"
+      );
+    }
+
+    res.status(200).json({ status: "success", data: reservations });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+const editReservation = async (req, res) => {
+  try {
+    const { reservationId, status } = req.body;
+
+    if (req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({
+          status: "error",
+          message: "Forbidden. Admin access required.",
+        });
+    }
+
+    const updatedReservation = await Reservation.findByIdAndUpdate(
+      reservationId,
+      { $set: { status } },
+      { new: true }
+    );
+
+    res.status(200).json({ status: "success", data: updatedReservation });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
 module.exports = {
   createReservation,
+  getReservations,
+  editReservation,
 };
--- backend/routes/reservationRoutes.js
+++ backend/routes/reservationRoutes.js
@@ -3,5 +3,6 @@ const reservationController = require("../controllers/reservationController");
 const authenticateUser = require("../middleware/authenticateUser");
 
 router.post("/", authenticateUser, reservationController.createReservation);
+router.get("/", authenticateUser, reservationController.getReservations);
 
 module.exports = router;
