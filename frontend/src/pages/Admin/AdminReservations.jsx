5e77f1b9aa44a06361c04172e1f7d4a27a22dbc5
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-31 11:19:30 +0200

    added reservations

--- frontend/src/pages/Admin/AdminReservations.jsx
+++ frontend/src/pages/Admin/AdminReservations.jsx
@@ -1,4 +1,32 @@
+import { useEffect } from "react";
+import useAxios from "../../hooks/useAxios";
+import ReservationCard from "../../components/ReservationCards/ReservationCard";
+
 const AdminReservations = () => {
-  return <div>AdminReservations</div>;
+  const {
+    runAxios: fetchReservations,
+    data: reservations,
+    loading,
+  } = useAxios("/reservation");
+
+  console.log(reservations);
+
+  useEffect(() => {
+    fetchReservations();
+  }, [fetchReservations]);
+
+  return (
+    <div className="h-screen overflow-y-auto">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+          {reservations.data.map((reservation) => (
+            <ReservationCard key={reservation._id} reservation={reservation} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 export default AdminReservations;
