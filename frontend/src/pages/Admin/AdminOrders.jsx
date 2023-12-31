5d7adfe223f6b3923751965ee6f8a1576eb1fe27
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-12-31 05:01:11 +0200

    fixed all orders issues with some conditions and ui

--- frontend/src/pages/Admin/AdminOrders.jsx
+++ frontend/src/pages/Admin/AdminOrders.jsx
@@ -43,9 +43,10 @@ const AdminOrders = () => {
 
   return (
     <div>
-      <label>
+      <label className="font-medium">
         Select Order Status:
         <select
+          className="ml-2 pe-9 border-gray-200 outline-none rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
           value={selectedStatus}
           onChange={(e) => setSelectedStatus(e.target.value)}
         >
@@ -57,12 +58,18 @@ const AdminOrders = () => {
         </select>
       </label>
 
-      <div className="h-screen overflow-y-auto">
-        {filteredOrders().map((order) => (
-          <div className="" key={order._id}>
-            <Order {...order} OrderStat={order.status} />
-          </div>
-        ))}
+      <div className="h-screen overflow-y-auto py-4">
+        {filteredOrders().length > 0 ? (
+          filteredOrders().map((order) => (
+            <div className="" key={order._id}>
+              <Order {...order} OrderStat={order.status} />
+            </div>
+          ))
+        ) : (
+          <p className="font-medium text-red-600">
+            No orders with this status.
+          </p>
+        )}
       </div>
     </div>
   );
