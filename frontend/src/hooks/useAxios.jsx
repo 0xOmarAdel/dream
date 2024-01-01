c73b035d8d09c81240f866b33b2b768cb23d954b
Heisenberg <omaradelawad20013@gmail.com>
2024-01-01 11:11:40 +0200

    made the default loading state true again and added "isExecuting" state

--- frontend/src/hooks/useAxios.jsx
+++ frontend/src/hooks/useAxios.jsx
@@ -3,12 +3,14 @@ import axiosApi from "../utils/axiosConfig";
 
 const useAxios = (url, method, body) => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [isExecuting, setIsExecuting] = useState(false);
   const [error, setError] = useState(false);
 
   const runAxios = useCallback(async () => {
     try {
       setLoading(true);
+      setIsExecuting(true);
       const response = await axiosApi.request({
         method: method || "GET",
         url,
@@ -19,10 +21,11 @@ const useAxios = (url, method, body) => {
       setError(!!error);
     } finally {
       setLoading(false);
+      setIsExecuting(false);
     }
   }, [method, url, body]);
 
-  return { runAxios, data, loading, error };
+  return { runAxios, data, loading, isExecuting, error };
 };
 
 export default useAxios;
