15235a0c33e5e68e4f33f5a1c3e7f3e71ac1626e
Heisenberg <omaradelawad20013@gmail.com>
2024-01-01 16:40:09 +0200

    fixed an error

--- frontend/src/components/Profile/ProfileSidebar.jsx
+++ frontend/src/components/Profile/ProfileSidebar.jsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 import { MdCalendarToday, MdFastfood, MdLogout } from "react-icons/md";
-import { MdCalendarToday, MdFastfood, MdLogout } from "react-icons/md";
 import { FaUserEdit } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/slices/userAuthSlice";
