a2438e6cbaa398d872087f6abd91464300c5c118
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-01 09:18:18 +0200

    change ui

--- frontend/src/pages/Profile/UserInfo.jsx
+++ frontend/src/pages/Profile/UserInfo.jsx
@@ -30,99 +30,97 @@ const UserInfo = ({ user }) => {
     <>
       <div className="min-h-screen flex justify-center">
         <div className="flex-col lg:flex-row-reverse">
-          <div className="card shrink-0 w-full max-w-sm">
-            <form className="card-body">
-              <div className="form-control">
-                <div className="grid grid-cols-1 lg:grid-cols-2  gap-2">
-                  <div className="mb-4">
-                    <label
-                      className="block text-gray-700 text-sm font-bold mb-2"
-                      htmlFor="name"
-                    >
-                      First Name
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      value={editedUser.name}
-                      onChange={handleInputChange}
-                      className="w-full px-3 py-2 border rounded-md"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label
-                      className="block text-gray-700 text-sm font-bold mb-2"
-                      htmlFor="name"
-                    >
-                      Last Name
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      value={editedUser.name}
-                      onChange={handleInputChange}
-                      className="w-full px-3 py-2 border rounded-md"
-                    />
-                  </div>
-                  <div className="mb-4 lg:col-span-2">
-                    <label
-                      className="block text-gray-700 text-sm font-bold mb-2"
-                      htmlFor="email"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      value={editedUser.email}
-                      onChange={handleInputChange}
-                      className="w-full px-3 py-2 border rounded-md"
-                    />
-                  </div>
-                  <div className="mb-4 lg:col-span-2">
-                    <label
-                      className="block text-gray-700 text-sm font-bold mb-2"
-                      htmlFor="password"
-                    >
-                      Current Password
-                    </label>
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      value={editedUser.password}
-                      onChange={handleInputChange}
-                      className="w-full px-3 py-2 border rounded-md"
-                    />
-                  </div>
-                  <div className="mb-4 lg:col-span-2">
-                    <label
-                      className="block text-gray-700 text-sm font-bold mb-2"
-                      htmlFor="password"
-                    >
-                      New Password
-                    </label>
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      value={editedUser.password}
-                      onChange={handleInputChange}
-                      className="w-full px-3 py-2 border rounded-md"
-                    />
-                  </div>
-                  <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md"
-                    onClick={handleSaveChanges}
+          <div className="w-full">
+            <div className="form-control">
+              <div className="grid grid-cols-1 lg:grid-cols-2  gap-2">
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="name"
                   >
-                    Save Changes
-                  </button>
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={editedUser.name}
+                    onChange={handleInputChange}
+                    className="w-full px-3 py-2 border rounded-md"
+                  />
                 </div>
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="name"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={editedUser.name}
+                    onChange={handleInputChange}
+                    className="w-full px-3 py-2 border rounded-md"
+                  />
+                </div>
+                <div className="mb-4 lg:col-span-2">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={editedUser.email}
+                    onChange={handleInputChange}
+                    className="w-full px-3 py-2 border rounded-md"
+                  />
+                </div>
+                <div className="mb-4 lg:col-span-2">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="password"
+                  >
+                    Current Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={editedUser.password}
+                    onChange={handleInputChange}
+                    className="w-full px-3 py-2 border rounded-md"
+                  />
+                </div>
+                <div className="mb-4 lg:col-span-2">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="password"
+                  >
+                    New Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={editedUser.password}
+                    onChange={handleInputChange}
+                    className="w-full px-3 py-2 border rounded-md"
+                  />
+                </div>
+                <button
+                  className="bg-sky-500 hover:bg-sky-600 text-white py-2 px-4 rounded-md"
+                  onClick={handleSaveChanges}
+                >
+                  Save Changes
+                </button>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
