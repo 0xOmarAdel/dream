b993d68c3d5696735d128e9916774eab76c0cce8
Heisenberg <omaradelawad20013@gmail.com>
2024-01-01 07:55:38 +0200

    changed the loading component styles

--- frontend/src/ui/Loading.jsx
+++ frontend/src/ui/Loading.jsx
@@ -3,8 +3,8 @@ import BurgerAnimation from "../../src/assets/BurgerAnimation.json";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center">
-      <Lottie animationData={BurgerAnimation} loop={true} />
+    <div className="absolute inset-0 z-[100] w-full min-h-screen bg-gray-50 flex justify-center items-center">
+      <Lottie animationData={BurgerAnimation} loop={true} className="w-36" />
     </div>
   );
 }
