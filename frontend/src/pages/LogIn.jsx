8be041492b7d413abce0ca766342526e2cb60dd7
Heisenberg <omaradelawad20013@gmail.com>
2024-01-03 05:22:33 +0200

    updated the login page

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,10 +1,13 @@
 import { useDispatch } from "react-redux";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import { login } from "../store/slices/userAuthSlice";
 import { logInSchema } from "../schemas/loginSchema";
-import { Link } from "react-router-dom";
 import Lottie from "lottie-react";
 import chef from "../assets/chef.json";
+import FormikField from "../ui/FormikField";
+import Button from "../ui/Button";
+import { Link } from "react-router-dom";
+import Divider from "../ui/Divider";
 
 const LogIn = () => {
   const dispatch = useDispatch();
@@ -19,80 +22,71 @@ const LogIn = () => {
   };
 
   return (
-    <div className="flex justify-center items-center w-full bg-white px-5 py-5">
-      {/* Animation */}
-      <div className="hidden md:flex">
-        <Lottie animationData={chef} className="min-h-screen" />
-      </div>
+    <div
+      className="min-h-[80vh] flex justify-center items-center w-full bg-white px-5 py-5"
+      style={{
+        backgroundImage:
+          "url(https://cdn.myshoptet.com/usr/www.gymclothes.cz/user/documents/upload/blog2/co%20je%20to%20cico%20dieta.jpg)",
+      }}
+    >
+      <div className="items-center justify-center glass rounded-xl">
+        <div className="grid grid-cols-2 items-center">
+          <Lottie
+            animationData={chef}
+            className="col-span-1 h-full hidden md:block bg-primary rounded-ss-xl rounded-es-xl"
+          />
 
-      {/* Login Form */}
-      <div className="mx-auto md:pb-12 w-fit h-fit md:py-0 items-center justify-center glass border border-slate-100 rounded-md p-12 shadow-lg backdrop:filter backdrop:blur-sm relative">
-        <h1 className="text-start text-4xl sm:text-3xl font-semibold text-sky-500 my-6">
-          Sign In
-        </h1>
-
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-          }}
-          validationSchema={logInSchema}
-          onSubmit={(values) => {
-            handleLogin(values);
-          }}
-        >
-          <Form className="flex flex-col gap-5">
-            {/* Email field */}
-            <Field
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-              className="input input-bordered input-primary w-full max-w-xs"
-            />
-            {/* Email error message */}
-            <ErrorMessage name="email" />
-
-            {/* Password field */}
-            <Field
-              id="password"
-              name="password"
-              placeholder="***********"
-              type="password"
-              className="input input-bordered input-primary w-full max-w-xs"
-            />
-            {/* Password error message */}
-            <ErrorMessage name="password" />
-
-            {/* Submit button */}
-            <button
-              type="submit"
-              className="btn btn-active hover:!bg-sky-500 btn-primary !text-black btn-block max-w-[200px]"
+          <div className="col-span-2 md:col-span-1 p-14 flex flex-col gap-6">
+            <h1 className="text-start text-4xl sm:text-3xl font-semibold text-primary">
+              Sign In
+            </h1>
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validationSchema={logInSchema}
+              onSubmit={(values) => {
+                submitHandler(values);
+              }}
             >
-              Sign in
-            </button>
-
-            {/* Forgot password */}
-            <div>
-              <p className="text-center">
-                Forgot your password?{" "}
-                <Link to="/resetpassword" className="text-sky-500">
-                  Reset Password
-                </Link>
-              </p>
-            </div>
-
-            {/* Sign up */}
-            <div>
-              <p className="text-center">
-                Don't have an account?{" "}
-                <Link to="/register" className="text-sky-500">
-                  Sign Up
-                </Link>
-              </p>
-            </div>
-          </Form>
-        </Formik>
+              {({ errors, touched, isValid }) => (
+                <Form className="flex flex-col gap-6">
+                  <FormikField
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    error={errors.email}
+                    touched={touched.email}
+                    inputClasses="border-gray-400"
+                  />
+                  <FormikField
+                    name="password"
+                    type="password"
+                    placeholder="*******"
+                    error={errors.password}
+                    touched={touched.password}
+                    inputClasses="border-gray-400"
+                  />
+                  <Button
+                    type="submit"
+                    text="Submit"
+                    disabled={!isValid}
+                    classes="mt-2"
+                  />
+                </Form>
+              )}
+            </Formik>
+            <Divider />
+            <p className="text-center">
+              Don&apos;t have an account?{" "}
+              <Link to="/register" className="text-primary">
+                Sign Up
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
