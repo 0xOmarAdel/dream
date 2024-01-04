8ed6691d35dac24f4bd77ce848ac884bcdf1eb46
Heisenberg <omaradelawad20013@gmail.com>
2024-01-04 19:22:50 +0200

    used the query-string package in the useAxios hook

--- frontend/src/hooks/useAxios.jsx
+++ frontend/src/hooks/useAxios.jsx
@@ -1,7 +1,8 @@
 import { useState, useCallback } from "react";
 import axios from "axios";
+import queryString from "query-string";
 
-const useAxios = (url, method, body, searchParams) => {
+const useAxios = (url, method = "GET", body = null, searchParams = {}) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [isExecuting, setIsExecuting] = useState(false);
@@ -13,12 +14,12 @@ const useAxios = (url, method, body, searchParams) => {
 
       setLoading(true);
       setIsExecuting(true);
+      
       const response = await axios({
-        method: method || "GET",
-        url: `http://localhost:5000/api/v1${url}${
-          searchParams ? `?${searchParams}` : ""
-        }`,
+        method: method,
+        url: `http://localhost:5000/api/v1${url}`,
         data: body,
+        params: queryString.parse(searchParams),
         headers: {
           "Content-Type": "application/json",
           ...(localToken && { Authorization: `Bearer ${localToken}` }),
