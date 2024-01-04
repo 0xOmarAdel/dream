46088d6cec078e9370c07380f03806590b460306
Heisenberg <omaradelawad20013@gmail.com>
2024-01-05 01:53:39 +0200

    fixed the lag issue in the menu price filter

--- frontend/src/components/MenuFilters/MenuPriceFilter.jsx
+++ frontend/src/components/MenuFilters/MenuPriceFilter.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ReactSlider from "react-slider";
 
 const MenuPriceFilter = ({
@@ -7,9 +8,16 @@ const MenuPriceFilter = ({
   setSelectedMinPrice,
   setSelectedMaxPrice,
 }) => {
-  const minPrice = selectedMinPrice || priceRange[0];
-  const maxPrice = selectedMaxPrice || priceRange[1];
+  const minPrice = parseInt(selectedMinPrice || priceRange[0]);
+  const maxPrice = parseInt(selectedMaxPrice || priceRange[1]);
   const minDistance = Math.ceil((priceRange[1] - priceRange[0]) / 10);
+
+  const [priceValues, setPriceValues] = useState([minPrice, maxPrice]);
+
+  useEffect(() => {
+    setSelectedMinPrice(priceValues[0]);
+    setSelectedMaxPrice(priceValues[1]);
+  }, [priceValues, setSelectedMaxPrice, setSelectedMinPrice]);
 
   return (
     <div className="collapse collapse-arrow border-b border-gray-300 rounded-none">
@@ -22,11 +30,8 @@ const MenuPriceFilter = ({
           trackClassName="slider-track"
           pearling
           defaultValue={[priceRange.minPrice, priceRange.maxPrice]}
-          value={[minPrice, maxPrice]}
-          onChange={(newValues) => {
-            setSelectedMinPrice(newValues[0]);
-            setSelectedMaxPrice(newValues[1]);
-          }}
+          value={priceValues}
+          onChange={(newValues) => setPriceValues(newValues)}
           ariaLabel={["Lower thumb", "Upper thumb"]}
           minDistance={minDistance}
           step={1}
@@ -37,8 +42,10 @@ const MenuPriceFilter = ({
             className="w-fit bg-transparent border border-gray-300 rounded-sm text-slate-900 text-center font-semibold tracking-wider transition duration-700 focus:outline-none focus:border-gray-500"
             min={priceRange[0]}
             max={maxPrice - minDistance}
-            value={minPrice}
-            onChange={(e) => setSelectedMinPrice(e.target.value)}
+            value={priceValues[0]}
+            onChange={(e) =>
+              setPriceValues((prevState) => [+e.target.value, prevState[1]])
+            }
           />
           <span className="text-lg select-none">-</span>
           <input
@@ -46,8 +53,10 @@ const MenuPriceFilter = ({
             className="w-fit bg-transparent border border-gray-300 rounded-sm text-slate-900 text-center font-semibold tracking-wider transition duration-700 focus:outline-none focus:border-gray-500"
             min={minPrice + minDistance}
             max={priceRange[1]}
-            value={maxPrice}
-            onChange={(e) => setSelectedMaxPrice(e.target.value)}
+            value={priceValues[1]}
+            onChange={(e) =>
+              setPriceValues((prevState) => [prevState[0], +e.target.value])
+            }
           />
         </div>
       </div>
