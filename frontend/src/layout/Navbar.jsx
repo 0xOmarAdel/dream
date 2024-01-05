6a1f1b3d35b3056aaf8bcb2f12b2db7ff19f5108
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-05 07:31:36 +0200

    conditions in navbar, only if user loggedin show cart view

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -105,29 +105,46 @@ const Navbar = () => {
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
-                <span className="badge badge-sm indicator-item">
-                  {totalQuantity}
-                </span>
+                {isLoggedIn && (
+                  <span className="badge badge-sm indicator-item">
+                    {totalQuantity}
+                  </span>
+                )}
               </div>
             </div>
-            <div
-              tabIndex={0}
-              className="mt-3 z-50 card card-compact dropdown-content w-52 bg-base-100 shadow"
-            >
-              <div className="card-body">
-                <span className="font-bold text-lg">{totalQuantity} Items</span>
-                <span className="text-info">
-                  Subtotal: {priceFormatter(totalPrice)}
-                </span>
-                <div className="card-actions">
-                  <Button
-                    text="view cart"
-                    onClick={() => navigate("/cart")}
-                    classes="w-full"
-                  />
+            {isLoggedIn ? (
+              <div
+                tabIndex={0}
+                className="mt-3 z-50 card card-compact dropdown-content w-52 bg-base-100 shadow"
+              >
+                <div className="card-body">
+                  <span className="font-bold text-lg">
+                    {totalQuantity} Items
+                  </span>
+                  <span className="text-info">
+                    Subtotal: {priceFormatter(totalPrice)}
+                  </span>
+                  <div className="card-actions">
+                    <Button
+                      text="view cart"
+                      onClick={() => navigate("/cart")}
+                      classes="w-full"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
+            ) : (
+              <div
+                tabIndex={0}
+                className="mt-3 z-50 card card-compact dropdown-content w-52 bg-base-100 shadow"
+              >
+                <div className="card-body">
+                  <span className="font-medium text-md">
+                    Please log in to view your shopping cart.
+                  </span>
+                </div>
+              </div>
+            )}
           </div>
         )}
         <div className="dropdown dropdown-end">
