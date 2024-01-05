e2051089a387f6354bf3d3d90f793bf01a78a4d5
Heisenberg <omaradelawad20013@gmail.com>
2024-01-05 02:34:40 +0200

    updated search input style

--- frontend/src/components/MenuFilters/MenuFilters.jsx
+++ frontend/src/components/MenuFilters/MenuFilters.jsx
@@ -111,7 +111,7 @@ const MenuFilters = ({
       <MenuApplyFiltersButton applyFilters={applyFilters} />
       <MenuResetFiltersButton resetFilter={resetFilter} />
       <input
-        className="w-full px-3 py-2 bg-transparent border outline-none rounded-md transition duration-300 focus:border-primary"
+        className="w-full px-3 py-2 bg-white border outline-none rounded-md transition duration-300 focus:border-primary"
         placeholder="Search.."
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
