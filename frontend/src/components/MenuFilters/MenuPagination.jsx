86974e79419ee916b3e65fec6350ad23fe246a02
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-05 07:23:32 +0200

    added entries pagination

--- frontend/src/components/MenuFilters/MenuPagination.jsx
+++ frontend/src/components/MenuFilters/MenuPagination.jsx
@@ -9,47 +9,58 @@ const MenuPagination = ({
   totalMeals,
   totalPages,
 }) => {
-  const numPagesToShow = 5;
+  const numPagesToShow = Math.min(totalPages, 5);
+
   const pageNumbers = Array.from(
     { length: numPagesToShow },
-    (_, index) => currentPage - 2 + index
+    (_, index) => currentPage - Math.floor(numPagesToShow / 2) + index
   );
 
+  const entriesPerPage = 24;
+  const startEntry = (currentPage - 1) * entriesPerPage + 1;
+  const endEntry = Math.min(currentPage * entriesPerPage, totalMeals);
+
   return (
-    <div className="flex items-center justify-center mt-4 font-medium">
-      {currentPage > 1 && (
-        <button
-          className="mr-2 px-4 py-2 bg-primary text-white rounded-full focus:outline-none hover:bg-sky-600"
-          onClick={() => setCurrentPage(currentPage - 1)}
-        >
-          <MdKeyboardDoubleArrowLeft />
-        </button>
-      )}
-      {pageNumbers.map(
-        (pageNumber) =>
-          pageNumber >= 1 &&
-          pageNumber <= totalPages && (
-            <button
-              key={pageNumber}
-              className={`mr-2 px-4 py-2 ${
-                pageNumber === currentPage
-                  ? "bg-sky-500 text-white rounded-full focus:outline-none hover:bg-sky-600"
-                  : "bg-gray-200 text-gray-700 rounded-full focus:outline-none hover:bg-gray-300"
-              }`}
-              onClick={() => setCurrentPage(pageNumber)}
-            >
-              {pageNumber}
-            </button>
-          )
-      )}
-      {currentPage < totalPages && (
-        <button
-          className="px-4 py-2 bg-sky-500 text-white rounded-full focus:outline-none hover:bg-sky-600"
-          onClick={() => setCurrentPage(currentPage + 1)}
-        >
-          <MdKeyboardDoubleArrowRight />
-        </button>
-      )}
+    <div className="flex items-center justify-center flex-col mt-4 font-medium">
+      <div>
+        {currentPage > 1 && (
+          <button
+            className="mr-2 px-4 py-2 text-lg font-bold bg-primary text-white rounded-full focus:outline-none hover:bg-sky-600"
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            <MdKeyboardDoubleArrowLeft />
+          </button>
+        )}
+        {pageNumbers.map(
+          (pageNumber) =>
+            pageNumber >= 1 &&
+            pageNumber <= totalPages && (
+              <button
+                key={pageNumber}
+                className={`mr-2 px-4 py-2 ${
+                  pageNumber === currentPage
+                    ? "bg-sky-500 text-white rounded-full focus:outline-none hover:bg-sky-600"
+                    : "bg-gray-200 text-gray-700 rounded-full focus:outline-none hover:bg-gray-300"
+                }`}
+                onClick={() => setCurrentPage(pageNumber)}
+              >
+                {pageNumber}
+              </button>
+            )
+        )}
+        {currentPage < totalPages && (
+          <button
+            className="px-4 py-2 bg-sky-500 text-white font-bold text-lg rounded-full focus:outline-none hover:bg-sky-600"
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            <MdKeyboardDoubleArrowRight />
+          </button>
+        )}
+      </div>
+
+      <div className="text-gray-600 mt-2">
+        Showing {startEntry} to {endEntry} of {totalMeals} entries
+      </div>
     </div>
   );
 };
