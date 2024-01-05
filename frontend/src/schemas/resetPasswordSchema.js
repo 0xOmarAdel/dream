7769a9efa6ad0b3ebd4c0baf4a02c951c7f11e34
Heisenberg <omaradelawad20013@gmail.com>
2024-01-05 09:42:33 +0200

    deleted resetPasswordSchema file

--- frontend/src/schemas/resetPasswordSchema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const resetPasswordSchema = yup.object().shape({
-    email: yup
-        .string()
-        .email("Invalid email")
-        .required("Required"),
-})
