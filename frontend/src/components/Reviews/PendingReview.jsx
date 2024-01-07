a925a6d2137f30fb130b73e8e4a5197fc5912dea
Heisenberg <omaradelawad20013@gmail.com>
2024-01-07 14:54:31 +0200

    added the star input in the pending review component

--- frontend/src/components/Reviews/PendingReview.jsx
+++ frontend/src/components/Reviews/PendingReview.jsx
@@ -1,14 +1,17 @@
-import Stars from "../../ui/Stars";
+import { useState } from "react";
 import MealImage from "../Meals/MealImage";
 import MealTitle from "../Meals/MealTitle";
+import StarInput from "../../ui/StarInput";
 
 const PendingReview = ({ meal }) => {
+  const [rating, setRating] = useState(0);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-6">
       <MealImage image={meal.image} classes="w-full h-32 object-fill" />
       <div className="flex flex-col gap-2">
         <MealTitle title={meal.title} classes="m-0 text-xl" />
-        <Stars max={5} numberOfStars={meal.rating} containerClasses="text-lg" />
+        <StarInput numberOfStars={5} setRating={setRating} />
       </div>
     </div>
   );
