7f6852c9c2b8b4292371d25c4c5d1d5761a1a8c4
Heisenberg <omaradelawad20013@gmail.com>
2024-01-07 14:45:16 +0200

    updated the reviewed meals and pending reviews components

--- frontend/src/components/Reviews/PendingReviews.jsx
+++ frontend/src/components/Reviews/PendingReviews.jsx
@@ -1,7 +1,38 @@
+import Stars from "../../ui/Stars";
+import MealImage from "../Meals/MealImage";
+import MealTitle from "../Meals/MealTitle";
+
 const PendingReviews = ({ meals }) => {
   console.log(meals);
 
-  return <div>PendingReviews</div>;
+  return (
+    <div
+      className={`grid ${
+        meals.length === 1
+          ? "grid-cols-1"
+          : meals.length > 1
+          ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-1"
+          : ""
+      } gap-x-10 gap-y-7`}
+    >
+      {meals.map((meal) => (
+        <div
+          key={meal._id}
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-6"
+        >
+          <MealImage image={meal.image} classes="w-full h-32 object-fill" />
+          <div className="flex flex-col gap-2">
+            <MealTitle title={meal.title} classes="m-0 text-xl" />
+            <Stars
+              max={5}
+              numberOfStars={meal.rating}
+              containerClasses="text-lg"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PendingReviews;
--- frontend/src/components/Reviews/ReviewedMeals.jsx
+++ frontend/src/components/Reviews/ReviewedMeals.jsx
@@ -1,7 +1,37 @@
+import MealImage from "../Meals/MealImage";
+import MealTitle from "../Meals/MealTitle";
+import Stars from "../../ui/Stars";
 const ReviewedMeals = ({ meals }) => {
   console.log(meals);
 
-  return <div>ReviewedMeals</div>;
+  return (
+    <div
+      className={`grid grid-cols-1 ${
+        meals.length === 1
+          ? "grid-cols-1"
+          : meals.length > 1
+          ? "sm:grid-cols-2"
+          : ""
+      } gap-x-10 gap-y-7`}
+    >
+      {meals.map((meal) => (
+        <div
+          key={meal._id}
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-6"
+        >
+          <MealImage image={meal.image} classes="w-full h-32 object-fill" />
+          <div className="flex flex-col gap-2">
+            <MealTitle title={meal.title} classes="m-0 text-xl" />
+            <Stars
+              max={5}
+              numberOfStars={meal.rating}
+              containerClasses="text-lg"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ReviewedMeals;
