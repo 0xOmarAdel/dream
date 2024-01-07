0e7ed0c089d52a20fcdae799c801001986cdc877
Heisenberg <omaradelawad20013@gmail.com>
2024-01-07 13:37:57 +0200

    added the reviews page to the navbar

--- frontend/src/data/userMenu.js
+++ frontend/src/data/userMenu.js
@@ -2,4 +2,5 @@ export const userMenu = [
   { id: 1, text: "profile", to: "/profile" },
   { id: 2, text: "orders", to: "/orders" },
   { id: 3, text: "reservations", to: "/reservations" },
+  { id: 4, text: "reviews", to: "/reviews" },
 ];
