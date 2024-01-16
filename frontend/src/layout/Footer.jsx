865b71dcb751b9c4c14bf5a769cac0542375f4c8
Heisenberg <omaradelawad20013@gmail.com>
2024-01-16 04:30:07 +0200

    updated the footer links

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="footer font-medium border-t p-10 bg-gray-50 text-base-content">
+    <footer className="footer py-10 px-8 sm:px-10 md:px-16 lg:px-16 xl:px-22 font-medium border-t bg-gray-50 text-base-content">
       <aside className="lg:px-4">
         <Link to="/" className="text-2xl text-sky-500 font-semibold">
           Dream
         </Link>
-        <p>
+        <p className="leading-7">
           A journey began with a passion for cooking.
           <br />
           Established in 2023
@@ -18,19 +18,21 @@ const Footer = () => {
         <Link to="/menu" className="link link-hover">
           Menu
         </Link>
-        <Link to="/reservations" className="link link-hover">
+        <a href="/#reservation" className="link link-hover">
           Reserve a Table
-        </Link>
-        <a href="#weekly" className="link link-hover">
+        </a>
+        <a href="/#specials" className="link link-hover">
           Weekly specials
         </a>
       </nav>
       <nav>
         <header className="footer-title opacity-100">Company</header>
-        <a id="#about-us" className="link link-hover">
+        <a id="#about" className="link link-hover">
           About us
         </a>
-        <a className="link link-hover">Contact</a>
+        <Link to="/contact" className="link link-hover">
+          Contact
+        </Link>
       </nav>
       <nav>
         <header className="footer-title opacity-100">Legal</header>
