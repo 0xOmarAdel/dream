fa415caacc0add342474704e1476ac91df00880f
Heisenberg <omaradelawad20013@gmail.com>
2024-01-16 04:29:56 +0200

    added an optional id prop to the section ui component

--- frontend/src/ui/Section.jsx
+++ frontend/src/ui/Section.jsx
@@ -1,10 +1,11 @@
 import { twMerge } from "tailwind-merge";
 import { forwardRef } from "react";
 
-const Section = forwardRef(({ children, classes }, ref) => {
+const Section = forwardRef(({ children, classes, id }, ref) => {
   return (
     <section
       ref={ref}
+      id={id || undefined}
       className={twMerge(
         "px-8 sm:px-10 md:px-16 lg:px-16 xl:px-22 py-12",
         classes || ""
